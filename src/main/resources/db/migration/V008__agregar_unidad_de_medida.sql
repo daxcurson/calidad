0239c6db172d465fb23088cdf0672061d9367caf
Cloud User <ec2-user@ip-172-31-29-70.us-west-2.compute.internal>
2017-06-19 19:45:43 -0400

    Modificado el script para modificar la creacion de la tabla de unidades de medida

--- src/main/resources/db/migration/V008__agregar_unidad_de_medida.sql
+++ src/main/resources/db/migration/V008__agregar_unidad_de_medida.sql
@@ -4,6 +4,6 @@ CREATE TABLE `calidad`.`unidades_medida`
 	`id` INT NOT NULL AUTO_INCREMENT , 
 	`unidad` VARCHAR(30) NOT NULL , 
 	`simbolo` VARCHAR(10) NOT NULL , 
-	`descripcion` VARCHAR NOT NULL , 
+	`descripcion` VARCHAR(200) NOT NULL , 
 	PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
